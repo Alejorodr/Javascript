016088ab2b33c40de4167c9cc3362de19afe0c76
Alejandro Rodriguez <kevinrodr@hotmail.com>
2023-09-21 21:36:38 -0300

    se agrega estilo de tarjetas, btn de modificar tarjeras y estilos

--- js/javascript.js
+++ js/javascript.js
@@ -1,124 +1,3 @@
-// //Simulador interactivo Segunda pre-entrega
-
-// // Definir notas en blanco para almacenar las notas
-// let notas = [];
-
-// // Función para mostrar el menú y obtener la selección del usuario
-// function mostrarMenu() {
-//     let opciones = [
-//         "1. Agregar nota ➕",
-//         "2. Mostrar notas 🗒️",
-//         "3. Eliminar nota ➖",
-//         "4. Extender nota 🔗",
-//         "5. Buscar notas 🔎",
-//         "6. Salir ❌"
-//     ];
-//     console.log(opciones.join("\n"));
-
-//     let opcion = prompt("Seleccione una opción:\n 1. Agregar nota ➕ \n 2. Mostrar nota 🗒️ \n 3. Eliminar nota ➖ \n 4. Concatenar nota 🔗 \n 5. Buscar notas 🔎 \n 6. Salir ❌");
-//     return opcion;
-// }
-
-// // Función para agregar una nota
-// function agregarNota() {
-//     let nota = prompt("Ingresa la nueva nota 📝: ");
-//     notas.push(nota);
-//     alert("Haz agregado una nueva nota, para ver las notas presiona 2. ✅");
-// }
-
-// // Función para eliminar una nota
-// function eliminarNota() {
-//     if (notas.length === 0) {
-//         alert("No hay notas para eliminar.");
-//         return;
-//     }
-
-//     let indice = parseInt(prompt("Ingresa el numero correspondiente a la nota a eliminar (1, 2, ...): ")) - 1;
-//     if (indice < 0 || indice >= notas.length) {
-//         alert(`Numero inválido.`);
-//         return;
-//     }
-
-//     let eliminada = notas.splice(indice, 1);
-//     alert(`Se eliminó la nota: ${eliminada} 🚮`);
-// }
-
-// // Función para concatenar una nota
-// function concatenarNota() {
-//     if (notas.length === 0) {
-//         alert("No hay notas para extender.");
-//         return;
-//     }
-
-//     let indice = parseInt(prompt("Ingresa el numero de la nota a extender (1, 2, ...): ")) - 1;
-//     if (indice < 0 || indice >= notas.length) {
-//         alert(`Nunero no inválido.`);
-//         return;
-//     }
-
-//     let texto = prompt("Ingresa el texto a sumar: ");
-//     notas[indice] += texto;
-//     alert(`Se extendio la nota: ${notas[indice]}`);
-// }
-
-// // Función para buscar notas
-// function buscarNotas() {
-//     let busqueda = prompt("Ingresa el texto de búsqueda en las notas: ");
-//     let notasEncontradas = notas.filter(nota => nota.includes(busqueda));
-
-//     if (notasEncontradas.length === 0) {
-//         alert(`No se encontraron notas que contengan "${busqueda}".`);
-//         return;
-//     }
-
-//     alert(`🔎 Notas encontradas:`);
-//     for (let i = 0; i < notasEncontradas.length; i++) {
-//         alert(i + 1 + ". " + notasEncontradas[i]);
-//     }
-// }
-
-// // Función para mostrar las notas
-// function mostrarNotas() {
-//     if (notas.length === 0) {
-//         alert("No haz agregado ninguna nota en esta sesion. 🤷🏻‍♂️");
-//     } else {
-//         alert("🗒️ Notas:");
-//         for (let i = 0; i < notas.length; i++) {
-//             alert(i + 1 + ". " + notas[i]);
-//         }
-//     }
-// }
-
-// // Ciclo principal de la aplicación
-// let continuar = true;
-// while (continuar) {
-//     let opcion = mostrarMenu();
-
-//     switch (opcion) {
-//         case "1":
-//             agregarNota();
-//             break;
-//         case "2":
-//             mostrarNotas();
-//             break;
-//         case "3":
-//             eliminarNota();
-//             break;
-//         case "4":
-//             concatenarNota();
-//             break;
-//         case "5":
-//             buscarNotas();
-//             break;
-//         case "6":
-//             continuar = false;
-//             alert("Cerrando notas.😢");
-//             break;
-//         default:
-//             alert("Opción inválida. Por favor, elige una opción válida.");
-//             break;
-//     }
-// }
 // Definir notas en blanco para almacenar las notas
 let notas = [];
 
@@ -162,6 +41,14 @@ function mostrarNotas() {
         let li = document.createElement('li');
         li.textContent = notas[i].titulo + ": " + notas[i].nota;
         li.style.backgroundColor = notas[i].color;
+
+        // Agregar botón de editar a cada nota
+        let editarBtn = document.createElement('button');
+        editarBtn.textContent = 'Editar ✏️';
+        editarBtn.addEventListener('click', function () {
+            editarNota(i);
+        });
+        li.appendChild(editarBtn);
 
         // Agregar botón de eliminar a cada nota
         let eliminarBtn = document.createElement('button');
@@ -215,6 +102,85 @@ function buscarNota() {
     }
 }
 
+// Función para editar una nota
+function editarNota(indice) {
+    // Obtener la nota actual
+    let notaActual = notas[indice];
+
+    // Mostrar un formulario con los detalles actuales de la nota
+    tituloInput.value = notaActual.titulo;
+    notaInput.value = notaActual.nota;
+
+    // Cambiar el controlador del botón para que actualice la nota en lugar de añadir una nueva
+    agregarBtn.removeEventListener('click', agregarNota);
+    agregarBtn.addEventListener('click', function() {
+        actualizarNota(indice);
+    });
+}
+
+// Función para actualizar una nota
+function actualizarNota(indice) {
+    // Actualizar la nota con los nuevos detalles
+    notas[indice].titulo = tituloInput.value;
+    notas[indice].nota = notaInput.value;
+
+    // Cambiar el controlador del botón para que añada una nueva nota en lugar de actualizar la existente
+    agregarBtn.removeEventListener('click', actualizarNota);
+    agregarBtn.addEventListener('click', agregarNota);
+
+    // Actualizar la lista de notas
+    mostrarNotas();
+}
+
 // Agregar un controlador de eventos al botón
 agregarBtn.addEventListener('click', agregarNota);
 buscarBtn.addEventListener('click', buscarNota);
+// Función para mostrar las notas
+function mostrarNotas() {
+    // Limpiar la lista existente
+    listaNotas.innerHTML = '';
+
+    // Crear una nueva tarjeta para cada nota
+    for (let i = 0; i < notas.length; i++) {
+        let card = document.createElement('div');
+        card.className = 'card';
+        card.style.backgroundColor = notas[i].color;
+
+        let cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        let cardTitle = document.createElement('h5');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = notas[i].titulo;
+
+        let cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.textContent = notas[i].nota;
+
+        // Agregar botón de editar a cada nota
+        let editarBtn = document.createElement('button');
+        editarBtn.textContent = 'Editar ✏️';
+        editarBtn.className = 'btn btn-primary';
+        editarBtn.addEventListener('click', function () {
+            editarNota(i);
+        });
+
+        // Agregar botón de eliminar a cada nota
+        let eliminarBtn = document.createElement('button');
+        eliminarBtn.textContent = 'Eliminar ➖';
+        eliminarBtn.className = 'btn btn-danger ml-2';
+        eliminarBtn.addEventListener('click', function () {
+            notas.splice(i, 1);
+            mostrarNotas();
+        });
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(editarBtn);
+        cardBody.appendChild(eliminarBtn);
+        
+        card.appendChild(cardBody);
+
+        listaNotas.appendChild(card);
+    }
+}
